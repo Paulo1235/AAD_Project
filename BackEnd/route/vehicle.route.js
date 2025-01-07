d077b738f9a975eee9a21f31f6fe3c0ae77b3d77
HugoPereira97 <138706199+HugoPereira97@users.noreply.github.com>
2025-01-07 19:40:24 +0000

    Update vehicle.route.js

--- BackEnd/route/vehicle.route.js
+++ BackEnd/route/vehicle.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
-const { AddVehicle, RemoveVehicle } = require("../controller/vehicle.controller");
+const { AddVehicle, RemoveVehicle, UpdateVehicle } = require("../controller/vehicle.controller");
 
 const vehicleRouter = express.Router();
 
 vehicleRouter.post("/add-vehicle", AddVehicle);
 
 vehicleRouter.delete("/remove-vehicle", RemoveVehicle);
+
+vehicleRouter.put("/update-vehicle", UpdateVehicle);
 
 module.exports = vehicleRouter;
